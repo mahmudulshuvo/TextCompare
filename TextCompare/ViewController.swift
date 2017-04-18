2677f4fee69bc20a8f5ab9277d0b699b029ce9d0
mahmudulshuvo <shuvo0201@gmail.com>
2017-04-18 21:39:00 +0600

    Initial Commit

---
+++ TextCompare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextCompare
+//
+//  Created by Shuvo on 4/18/17.
+//  Copyright © 2017 SHUVO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
